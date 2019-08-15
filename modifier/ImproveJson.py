f756fef95be1953df701a31f9b1e0dba911596ec
Ebrahim Karam <ebrahim_karam@hotmail.com>
2019-08-15 18:02:26 +0300

    Fixed the Second Json File

--- modifier/ImproveJson.py
+++ modifier/ImproveJson.py
@@ -2,22 +2,35 @@ import json
 import pandas as pd
 import codecs
 
-filename = "modifier\Lebanon_Level1.json"
+filename = "modifier\Lebanon_Level2.json"
+
 if filename:
-    with open(filename, 'r') as f:
+    with codecs.open(filename, 'r', 'utf-8') as f:
         datastore = json.load(f)
 
 mohafazaPairs = pd.read_excel(
     'modifier\English List of districts. Arabic names of districts 2.0.xlsx', sheet_name=3, index_col='In English')
 
+districtPairs = pd.read_excel(
+    'modifier\English List of districts. Arabic names of districts 2.0.xlsx', sheet_name=2, index_col='District')
 
-number_of_districts = len(datastore["objects"]["gadm36_LBN_1"]["geometries"])
+districtPairs = districtPairs.loc[~districtPairs.index.duplicated(
+    keep='first')]
+
+# Depending on fileName
+name = 'gadm36_LBN_2'
+number_of_districts = len(datastore["objects"][name]["geometries"])
 
 for i in range(0, number_of_districts):
-    print(datastore['objects']["gadm36_LBN_1"]
-          ["geometries"][i]['properties']['NAME_1'])
-    english_district = datastore['objects']["gadm36_LBN_1"]["geometries"][i]['properties']['NAME_1']
-    datastore['objects']["gadm36_LBN_1"]["geometries"][i]['properties']['Arabic_NAME_1'] = mohafazaPairs.loc[english_district, 'Mohafaza Name']
+    english_mohafaza = datastore['objects'][name]["geometries"][i]['properties']['NAME_1']
+    print(english_mohafaza)
+    datastore['objects'][name]["geometries"][i]['properties']['Arabic_NAME_1'] = str(mohafazaPairs.loc[english_mohafaza, 'Mohafaza Name'])
+
+    english_district = datastore['objects'][name]["geometries"][i]['properties']['NAME_2']
+    print(english_district)
+    datastore['objects'][name]["geometries"][i]['properties']['Arabic_NAME_2'] = districtPairs.loc[english_district, 'Column1']
+    print(districtPairs.loc[english_district, 'Column1'])
+
 
 with codecs.open(filename, 'w', "utf-8") as write_file:
-    json.dump(datastore,write_file)
+    json.dump(datastore, write_file)
